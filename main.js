473438e060a85fd316d6f537b16df5fb9403dbf7
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2023-11-01 22:34:04 -0600

    Update main.js

--- main.js
+++ main.js
@@ -31,16 +31,32 @@
 
 
 
-const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': 'f3cdceacf6msh71c0e4cff5be441p15ed62jsnf06564818a72',
-        'X-RapidAPI-Host': 'api-basketball.p.rapidapi.com'
-    }
-};
+const apiKey = 'f3cdceacf6msh71c0e4cff5be441p15ed62jsnf06564818a72';
+const apiUrl = 'https://free-nba.p.rapidapi.com/players?page=0&per_page=25';
+const btnApi = document.querySelector('#btn-api');
+const resultadosDiv = document.querySelector('#resultados');
 
-fetch("https://api-basketball.p.rapidapi.com/games", options)
-    .then(res => res.text())
-    .then(response => {
-        console.log(response)
+btnApi.addEventListener('click', () => {
+    fetch(apiUrl, {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': apiKey
+        }
     })
+        .then(response => response.json())
+        .then(data => {
+            resultadosDiv.innerHTML = ""; // Limpia cualquier contenido anterior
+            data.data.forEach(player => {
+                const playerDiv = document.createElement('div');
+                playerDiv.classList.add('player-card');
+                playerDiv.innerHTML = `
+                <p>Nombre: ${player.first_name} ${player.last_name}</p>
+                <p>Posición: ${player.position}</p>
+                <p>Equipo: ${player.team.full_name}</p>
+            `;
+                resultadosDiv.appendChild(playerDiv);
+            });
+        })
+        .catch(error => console.error(error));
+});
+
